e7e81a879f1ca8a963f35bc2a6c7bacc325e5665
Ryna Truong <rynatruong@outlook.com>
2020-06-26 13:34:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,7 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
+<<<<<<< HEAD
 def batch_badge_creator(arr_name)
   new_arr = []
   arr_name.each do |person|
@@ -10,6 +11,8 @@ def batch_badge_creator(arr_name)
   new_arr
 end
 
+=======
+>>>>>>> fb07c73ca03d0c8a4a00dabed9b08f649cb20790
 def assign_rooms(list_of_speakers)
   new_arr = []
   list_of_speakers.each.with_index(1) do |speaker, idx|
@@ -18,10 +21,18 @@ def assign_rooms(list_of_speakers)
   new_arr
 end
 
+<<<<<<< HEAD
 def printer(people)
   batch_badge_creator(people).each {|phrase| puts phrase}
   assign_rooms(people).each {|phrase| puts phrase}
   end
 
 
+=======
+def assign_rooms(speaker)
+  speaker.each.map do |person|
+   puts "Hello, #{person}! You'll be assigned to room !"
+ end
+end
+>>>>>>> fb07c73ca03d0c8a4a00dabed9b08f649cb20790
   
